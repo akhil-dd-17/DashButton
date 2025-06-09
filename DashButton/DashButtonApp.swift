abb99dfc58d71074a0c7706791f7ab361ab3b7e9
Akhil Dakinedi <dakinedi+1@gmail.com>
2025-06-08 20:53:16 -0700

    Initial Commit

---
+++ DashButton/DashButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashButtonApp.swift
+//  DashButton
+//
+//  Created by Akhil Dakinedi on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DashButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DashButtonTests/DashButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  DashButtonTests.swift
+//  DashButtonTests
+//
+//  Created by Akhil Dakinedi on 6/8/25.
+//
+
+import Testing
+@testable import DashButton
+
+struct DashButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
